9d92c1ac4ac029efdd0832ad4e82a0cdb1d2b3d2
kdangmai <kdangmai.official@gmail.com>
2024-05-02 14:52:32 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>This is Index</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
